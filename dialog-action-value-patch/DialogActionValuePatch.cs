8dea284f7378672794c6f8b79662118e290b47f0
Lucius <ellipsecurvexnynzn@gmail.com>
2026-09-10 22:00:41 +0900

    Add the license to the source code

--- dialog-action-value-patch/DialogActionValuePatch.cs
+++ dialog-action-value-patch/DialogActionValuePatch.cs
@@ -1,4 +1,12 @@
-﻿using FCS_extended;
+﻿/*--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Copyright (C) 2026 Lucius
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+using FCS_extended;
 using HarmonyLib;
 using System;
 using System.Collections.Generic;
